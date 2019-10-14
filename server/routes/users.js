01039477cd48828346e1f6c12d798c563491a90d
邵正裕 <497539962@qq.com>
2019-10-14 17:32:20 +0800

    commit

---
+++ server/routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+router.get('/get',function(req,res,next){
+      res.json({
+        status:"1"
+      })
+})
+
+module.exports = router;
