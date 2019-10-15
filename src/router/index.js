d842d9062af813aeda664e6ee229d4b1ebd036ce
shaozhengyu <497539962@qq.com>
2019-10-15 23:40:58 +0800

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+
 // import VueSocketio from 'vue-socket.io'
 //   Vue.use(VueSocketio, 'ws://localhost:8081')
 Vue.use(Router)
@@ -12,6 +14,12 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
 
     }
   ]
