1c45be8a1526a1bbfe9ec34e92a211a00a67c581
shaozhengyu <497539962@qq.com>
2019-12-08 09:27:47 +0800

    commit

--- src/components/chatComp.vue
+++ src/components/chatComp.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="chatComp">
-     <div v-for="(item,index) in allChat"  :key="index" @click="clickTo(item)"><span v-for="(item1,index) in item.chatter" :key="index">{{item1.userId}}</span>
+    <swipeout>
+      <swipeout-item transitionMode="follow" v-for="(item,index) in allChat" :currentClick="index"  :key="index">
+         <div slot="right-menu" >
+          <swipeout-button @click.native="onButtonClick('fav')" type="primary">{{'Yes'}}</swipeout-button>
+          <swipeout-button @click.native="onButtonClick('delete')" type="warn">{{'Right'}}</swipeout-button>
+        </div>
+     <div slot="content" style="height:100px"  @click="clickTo(item)"><span v-for="(item1,index) in item.chatter" :key="index">{{item1.userId}}</span>
      {{'('+item.chatId+')'}}</div>
+      </swipeout-item>
+      <!-- <swipeout-button>123</swipeout-button> -->
+     </swipeout>
   </div>
+  
 </template>
 <script>
+import Swipeout from '../components/swiperout/swipeout'
+import SwipeoutItem from '../components/swiperout/swipeout-item'
+import SwipeoutButton from '../components/swiperout/swipeout-button'
+// import swiperout from '../components/swiperout'
 export default {
+  components:{
+    Swipeout,
+    SwipeoutItem,
+    SwipeoutButton
+  },
   props:{
     allChat:{
       type:Array,
---
+++ src/components/swiperout/swipeout.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="vux-swipeout">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'swipeout'
+}
+</script>
+
+<style lang="scss">
+.vux-swipeout {
+  width: 100%;
+  overflow: hidden;
+}
+.vux-swipeout-item {
+  position: relative;
+}
+.vux-swipeout-button-box {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  font-size: 0;
+  text-align: right;
+}
+.vux-swipeout-button-box-left {
+  text-align: left;
+}
+.vux-swipeout-button-box  {
+   div{
+     height: 100%;
+   }
+  
+}
+.vux-swipeout-button {
+  height: 100%;
+  text-align: center;
+  font-size: 14px;
+  color:red;
+  border: none;
+}
+.vux-swipeout-content {
+  position: relative;
+  background: #ccc;
+}
+.vux-swipeout-content.vux-swipeout-content-animated {
+  transition: transform 0.2s;
+}
+.vux-swipeout-button-primary {
+  background-color: red;
+}
+.vux-swipeout-button-warn {
+  background-color: red;
+}
+.vux-swipeout-button-default {
+  background-color: red;
+}
+</style>
